38066bb83f738c5f1f75ba533d91b71df2eb48c5
lemonstener <deyanvasilev7@gmail.com>
2021-05-01 14:06:58 -0500

    lots of tests added

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///users_db_test'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -10,21 +10,23 @@ app.config['TESTING'] = True
 
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
-db.drop_all()
-db.create_all()
-
-
 class UserViewsTestCase(TestCase):
     """Tests for views for Users."""
 
     def setUp(self):
-        """Add sample user."""
+        """Drop and create all tables, add sample user."""
 
-        User.query.delete()
+        db.drop_all()
+        db.create_all()
 
         user = User(first_name="John", last_name='Doe')
         db.session.add(user)
         db.session.commit()
+
+        post = Post(title='Hi',content='Bye',user_id=1)
+        db.session.add(post)
+        db.session.commit()
+
 
         self.user_id = user.id
 
@@ -33,19 +35,133 @@ class UserViewsTestCase(TestCase):
 
         db.session.rollback()
 
-    def test_list_user(self):
+    def test_home_page(self):
         with app.test_client() as client:
-            resp = client.get("/")
-            html = resp.get_data(as_text=True)
+            '''Test the home page.'''
 
-            self.assertEqual(resp.status_code, 302)
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+            
+            ''' / redirects to /users.'''
+            self.assertEqual(res.status_code, 302)
+    
+    def test_home_page_followed(self):
+        with app.test_client() as client:
+            '''Redirection from home page followed.'''
+
+            res = client.get("/", follow_redirects=True)
+            html = res.get_data(as_text=True)
+            
+            
+            self.assertEqual(res.status_code, 200)
+            '''Tests that /users shows a list of all users'''
+            self.assertIn('<li><a href="/users/1">John Doe</a></li>',html)
             
 
     def test_show_user(self):
+        '''Tests the user page, make sure it displays the only right user and correct posts.'''
         with app.test_client() as client:
-            resp = client.get(f"/users/{self.user_id}")
-            html = resp.get_data(as_text=True)
+            res = client.get("/users/1")
+            html = res.get_data(as_text=True)
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('First name: John',html)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>John Doe</h1>',html)
+
+            '''User page displays all of the user's posts'''
+            self.assertIn('<li><a href="/posts/1">Hi</a></li>',html)
+    
+    def test_new_user(self):
+        '''New user redirection test'''
+        with app.test_client() as client:
+            res = client.post('/users/new',data={'first':'Jane','last':'Smith','url':''})
+            
+            self.assertEqual(res.status_code,302)
+            self.assertEqual(res.location,'http://localhost/users')
+    
+    def test_new_user_followed_success(self):
+        with app.test_client() as client:
+            res = client.post('/users/new',data={'first':'Jane','last':'Smith','url':''}, follow_redirects=True)
+            html = res.get_data(as_text=True)
+            
+            '''Test for a successfull creation of new user'''
+            self.assertEqual(res.status_code,200)
+            self.assertIn('Jane Smith',html)
+
+    def test_new_user_followed_fail(self):
+        with app.test_client() as client:
+            res = client.post('/users/new',data={'first':'Jane','last':'','url':''}, follow_redirects=True)
+            html = res.get_data(as_text=True)
+            
+            '''Test for failed new user creation, make sure page doesn't break, display flash messages'''
+            self.assertEqual(res.status_code,200)
+            self.assertNotIn('Jane',html)
+            self.assertIn('<p>First and last names cannot be empty</p>',html)
+    
+    
+    def test_post_page(self):
+        with app.test_client() as client:
+            '''Test a post's "home" page'''
+            res = client.get('/posts/1')
+            html = res.get_data(as_text=True)
+
+            self.assertIn('<h1>Hi</h1>',html)
+            self.assertIn('<p>Bye</p>',html)
+            self.assertIn('<blockquote>By John Doe</blockquote>',html)
+
+    def test_new_post(self):
+        with app.test_client() as client:
+            res = client.post('/users/1/posts/new',data={'title':'Hello','content':'Goodbye'})
+
+            self.assertEqual(res.status_code,302)
+            self.assertEqual(res.location,'http://localhost/users/1')
+    
+    def test_new_post_followed_success(self):
+        with app.test_client() as client:
+            res = client.post('/users/1/posts/new',data={'title':'Hello','content':'Goodbye'}, follow_redirects=True)
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code,200)
+            self.assertIn('Hello',html)
+
+    def test_new_post_followed_fail(self):
+        with app.test_client() as client:
+            res = client.post('/users/1/posts/new',data={'title':'Hello','content':''}, follow_redirects=True)
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code,200)
+            self.assertNotIn('Hello',html)
+    
+    def test_delete_user(self):
+        with app.test_client() as client:
+            res = client.get('/users/1/delete')
+            
+            '''Test redirection'''
+            self.assertEqual(res.status_code,302)
+            self.assertEqual(res.location,'http://localhost/users')
+
+    def test_delete_user_followed(self):
+        with app.test_client() as client:
+            res = client.get('/users/1/delete', follow_redirects=True)
+            html = res.get_data(as_text=True)
+            
+            self.assertEqual(res.status_code,200)
+            self.assertNotIn('<h1>John Doe</h1>',html)
+    
+    def test_delete_post(self):
+        with app.test_client() as client:
+            res = client.get('/posts/1/delete')
+        
+        '''Test redirection'''
+        self.assertEqual(res.status_code,302)
+        self.assertEqual(res.location,'http://localhost/users/1')
+
+    def test_delete_post_followed(self):
+        with app.test_client() as client:
+            res = client.get('/posts/1/delete', follow_redirects=True)
+            html = res.get_data(as_text=True)
+        
+        '''Make sure post is deleted, make sure it does not affect the user aside from the list contents.'''
+        self.assertEqual(res.status_code,200)
+        self.assertNotIn('<h1>Hi</h1>',html)
+        self.assertIn('<h1>John Doe</h1>',html)
 
--- test_models.py
+++ test_models.py
@@ -1,39 +1,56 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
-# Use test database and don't clutter tests with SQL
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///users_db_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
-db.drop_all()
-db.create_all()
 
-
-class UserModelTestCase(TestCase):
+class UserPostModelsTestCase(TestCase):
     """Tests for the User model."""
 
     def setUp(self):
         """Clean up any existing users"""
-
-        User.query.delete()
+        db.drop_all()
+        db.create_all()
 
     def tearDown(self):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
 
-    def test_fullname(self):
-        '''Test the full_name() function.'''
-        user = User(first_name="John", last_name="Doe")
-        self.assertEqual(user.full_name(), "John Doe")
-
-    def test_image_url(self):
-        '''Make sure default image works.'''
+    def test_user(self):
+        '''Multitude of different tests on the User model'''
         user = User(first_name="John", last_name="Doe")
         db.session.add(user)
         db.session.commit()
-        self.assertEquals(user.image_url,'https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png')
 
-       
+        '''Test the full_name() function.'''
+        self.assertEqual(user.full_name(), "John Doe")
+
+        '''Test for default values'''
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.image_url,'https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png')
+
+    def test_user_relation(self):
+        '''Tests that the post belongs to right user'''
+        user = User(first_name="John",last_name='Doe')
+        db.session.add(user)
+        db.session.commit()
+
+        post = Post(title="Hi",content="Bye",user_id=1)
+        db.session.add(post)
+        db.session.commit()
+        
+        '''Testing happens through the backref property'''
+        self.assertEqual(post.user.first_name, 'John')
+        self.assertEqual(post.user.id, 1)
+    
+        
+    
+    
+
+    
+
